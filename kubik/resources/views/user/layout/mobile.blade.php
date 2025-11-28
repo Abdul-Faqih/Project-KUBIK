3e57a504e817b0de56f6d89865e1af0366c45eca
Abdul <ahmad.abdul@student.pradita.ac.id>
2025-11-28 10:24:35 +0700

    mau lanjut home cik

--- kubik/resources/views/user/layout/mobile.blade.php
+++ kubik/resources/views/user/layout/mobile.blade.php
@@ -6,12 +6,15 @@
     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
     <title>@yield('title')</title>
 
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+
     @vite('resources/css/app.css')
 
     <style>
         body {
             background: #fbfbfb;
-            font-family: 'Inter', sans-serif;
+            font-family: 'Poppins';
         }
 
         .mobile-wrapper {
